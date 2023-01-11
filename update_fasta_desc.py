af41e2c8495ff641dffaab8435903b3ff5d881b6
Arafat Rahman <arafat.rahman@email.ucr.edu>
2023-01-11 12:01:50 -0800

    Update update_fasta_desc.py

--- update_fasta_desc.py
+++ update_fasta_desc.py
@@ -1,17 +1,25 @@
-from Bio import SeqIO
-from sys import argv
-
 '''
 What it does:
+- Use provided pattern to split fasta description
 - Remove contig number from seqID
 - Only keep longest sequence if there are multiple entry for same seqID
 
+
 How to use this script in command line:
-python update_fasta_desc.py <fasta_file_input> <fasta_file_output>
+python update_fasta_desc.py <fasta_file_input> <split_symbol> <position_to_keep> <fasta_file_output>
+
+For example, let's say a fasta description is the following:
+
+>05LoS16R10_36_00608__05LoS16R10_36
+
+Here the split symbol will be '__' and position to keep will be 1 (count started from 0)
 '''
 
+from Bio import SeqIO
+from sys import argv
 
-def update_fasta_id(fasta_file):
+
+def update_fasta_id(fasta_file, split_symbol, position_to_keep):
         '''
         INPUT: fasta file
         Output: fasta file contianing id without the contig id
@@ -19,8 +27,8 @@ def update_fasta_id(fasta_file):
         seq_list = {}
         for seq_record in SeqIO.parse(fasta_file, 'fasta'):
                 seq_id = seq_record.id
-                if '_' in seq_id:
-                        seq_record.id = seq_id.split('_', -1)[0]
+                if split_symbol in seq_id:
+                        seq_record.id = seq_id.split(split_symbol)[position_to_keep]
 
                 if seq_record.id not in seq_list.keys():
                         seq_list[seq_record.id] = seq_record
@@ -31,5 +39,5 @@ def update_fasta_id(fasta_file):
 
 
 if __name__ == "__main__":
-        updated_seq_list = update_fasta_id(argv[1])
-        SeqIO.write(updated_seq_list.values(), argv[2], 'fasta')
+        updated_seq_list = update_fasta_id(argv[1], argv[2], argv[3])
+        SeqIO.write(updated_seq_list.values(), argv[4], 'fasta')
