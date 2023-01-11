06eb03354a156ccfc2f091417c0f1cbcaba92738
Arafat Rahman <arafat.rahman@email.ucr.edu>
2023-01-11 10:54:49 -0800

    Update get_fasta_stat.py

--- get_fasta_stat.py
+++ get_fasta_stat.py
@@ -21,15 +21,16 @@ def genome_size(fasta_dir, fasta_suffix):
   for fasta_file in listdir(fasta_dir):
     if fasta_file.endswith(fasta_suffix):
       for seq_record in SeqIO.parse(fasta_file, 'fasta'):
-        fasta_size[fasta_file] += len(str(seq_record.seq))
+        if fasta_file not in fasta_size.keys():
+         fasta_size[fasta_file] = int(len(str(seq_record.seq)))
+        else:
+          fasta_size[fasta_file] += int(len(str(seq_record.seq)))
     
   with open('fasta_stat.csv', 'w') as f:
     for key in fasta_size.keys():
-      f.write(f"{key}, {fasta_size[key]}")
+      f.write(f"{key}, {fasta_size[key]}\n")
   pass
 
 
 if __name__ == "__main__":
   genome_size(argv[1], argv[2])
-
-    
